6c9601ec23687df7d57fd44efd05c785038d73e7
awasthishashank <awasthishashank@gmail.comgit config --global user.username awasthishashank git config --global user.email awasthishashank@gmail.com>
2024-05-20 23:20:18 +0530

    Loginmandatory to see product

--- src/App.js
+++ src/App.js
@@ -11,17 +11,29 @@ import { Route } from "react-router-dom";
 import ProductDetail from "./components/Product/ProductDetail";
 import AuthForm from "./Pages/AuthForm";
 import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+import { useContext } from "react";
+import AuthContext from "./store/Auth-Context";
+import ProductItem from "./components/Product/ProductItem";
+import { Redirect } from 'react-router-dom/cjs/react-router-dom';
 
 function App() {
+  const authCtx = useContext(AuthContext)
   return (
   <CartProvider>
     <Header />
     <Switch>
     <Route path="/"exact ><Home /></Route>
+    {!authCtx.isLoggedIn && <Route path='/auth'>
+          <AuthForm />
+        </Route>}
+        <Route path='/store'>
+          {authCtx.isLoggedIn && <ProductItem/>}
+          {!authCtx.isLoggedIn && < Redirect to = '/auth'/>}
+        </Route>
     <Route path="/about"><About /></Route>
     <Route path="/auth"><AuthForm /></Route>
     <Route path="/contact"><AddContact /></Route>
-    <Route exact path="/store" ><ProductItems /></Route>
+    {/* <Route exact path="/store" ><ProductItems /></Route> */}
     <Route path="/cart" ><CartSection /></Route>
     <Route path="/store/:productId"><ProductDetail/></Route>
   </Switch>
